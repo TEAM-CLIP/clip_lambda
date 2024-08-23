7c3a41894a1e733cac653a3fbbb4e13ef5917af5
Sim-km <simkm0824@naver.com>
2024-08-23 13:37:51 +0900

    refactor: mysql 커넥션 생성 함수 cursor close 시점 변경

--- common/mysql_connector.py
+++ common/mysql_connector.py
@@ -14,24 +14,25 @@ class MysqlConnector:
 
 
     def __delete__(self, instance):
+        self.__cursor.close()
         self.__conn.close()
 
     def find_one(self, query, params):
         cursor = self.__cursor
         cursor.execute(query, params)
         result = cursor.fetchone()
-        cursor.close()
         return result
 
     def find_all(self, query, params):
         cursor = self.__cursor
         cursor.execute(query, params)
         result = cursor.fetchall()
-        cursor.close()
         return result
 
     def execute_one(self, query, params):
         cursor = self.__cursor
+        if params is None:
+            cursor.execute(query)
         cursor.execute(query, params)
         self.__conn.commit()
         return cursor.lastrowid
